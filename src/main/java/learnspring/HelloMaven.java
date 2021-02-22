f38f856ddbf8465dfa63f4c061ee04f221ea83f5
YOhannes <johnkelati16@gmail.com>
2021-02-21 22:35:22 -0600

    firstupdated2

---
+++ src/main/java/learnspring/HelloMaven.java
@@ -0,0 +1,15 @@
+package learnspring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMaven {
+
+    private static final Logger log= LoggerFactory.getLogger(HelloMaven.class);
+    public static void main(String args[]){
+       log.info("helloInfo");
+       log.debug("hellodebug");
+       log.debug("helloagain");
+
+    }
+}
